9e7182e9f690da057303d7b531f86aeeedee4385
apylers <apylers@gmail.com>
2021-01-18 20:43:56 +0800

    重新格式化排版代码

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # 中国滑稽大学(University of Ridiculous of China)健康打卡平台自动打卡脚本
 
-![Auto-report action](https://github.com/Violin9906/USTC-ncov-AutoReport/workflows/Auto-report%20action/badge.svg?branch=master&event=schedule)
+![Auto-Report](https://github.com/apylers/USTC-ncov-AutoReport/workflows/Auto-report%20action/badge.svg)
 ![School](https://img.shields.io/badge/School-URC-blue.svg)
 ![Language](https://img.shields.io/badge/language-Python3-yellow.svg)
-![GitHub stars](https://img.shields.io/github/stars/Violin9906/USTC-ncov-AutoReport)
-![GitHub forks](https://img.shields.io/github/forks/Violin9906/USTC-ncov-AutoReport)
 
 ## 说明
 
@@ -12,34 +10,35 @@
 
 ## 更新记录
 
-- 20200827：增加打卡失败重试功能，增加License。
-- 20200826：为配合学校最新规定，切换至Github Actions实现一天三次打卡
+- 20210118：重新格式化排版代码，去除多余要素，适配新年。
+- 20200827：增加打卡失败重试功能，增加 License。
+- 20200826：为配合学校最新规定，切换至 Github Actions 实现一天三次打卡
 
 ## 使用方法
 
-1. 将本代码仓库fork到自己的github。
+1. 将本代码仓库 Fork 到自己的 GitHub。
 
-2. 根据自己的实际情况修改data.json的数据，参看下文。**这里给出的 `data.json` 仅供参考。**
+2. 根据自己的实际情况修改 `data.json` 的数据，参看下文。**这里给出的 `data.json` 仅供参考。**
 
-3. 将修改好的代码push至master分支。如果不需要修改 `data.json`，请在 `README.md` 里添加一个空格并push，否则不会触发之后的步骤。
+3. 将修改好的代码 push 至 master 分支。如果不需要修改 `data.json`，请在 `README.md` 里添加一个空格并 push，否则不会触发之后的步骤。
 
-4. 点击Actions选项卡，点击"I understand my workflows, go ahead and enable them".
+4. 点击 Actions 选项卡，点击 "I understand my workflows, go ahead and enable them"。
 
-5. 点击Settings选项卡，点击左侧Secrets，点击New secret，创建名为`STUID`，值为自己学号的secret。用同样方法，创建名为`PASSWORD`，值为自己中国滑稽大学统一身份认证密码的secret。这两个值不会被公开。
+5. 点击 Settings 选项卡，点击左侧 Secrets，点击 New secret，创建名为 `STUID`，值为自己学号的 secret。用同样方法，创建名为 `PASSWORD`，值为自己中国滑稽大学统一身份认证密码的 secret。**这两个值不会被公开。**
 
-   ![secrets](imgs/image-20200826215037042.png)
+   ![](imgs/secrets.png)
 
-6. 默认的打卡时间是每天的早上7:30、中午12:30和晚上19:30，可能会有数分钟的浮动。如需选择其它时间，可以修改`.github/workflows/report.yml`中的`cron`，详细说明参见[安排的事件](https://docs.github.com/cn/actions/reference/events-that-trigger-workflows#scheduled-events)，请注意这里使用的是**国际标准时间UTC**，北京时间的数值比它大8个小时。
+6. 默认的打卡时间是每天的早上 7: 30、中午 12: 30 和晚上 19: 30，可能会有数分钟的浮动。如需选择其它时间，可以修改 `.github/workflows/report.yml`中的 `cron`，详细说明参见[安排的事件](https://docs.github.com/cn/actions/reference/events-that-trigger-workflows#scheduled-events)，请注意这里使用的是**国际标准时间 UTC**，北京时间的数值比它大 8 个小时。
 
-7. 在Actions选项卡可以确认打卡情况。如果打卡失败（可能是临时网络问题等原因），脚本会自动重试，三次尝试后如果依然失败，将返回非零值提示构建失败。
+7. 在 Actions 选项卡可以确认打卡情况。如果打卡失败（可能是临时网络问题等原因），脚本会自动重试，三次尝试后如果依然失败，将返回非零值提示构建失败。
 
-8. 在Github个人设置页面的Notifications下可以设置Github Actions的通知，建议打开Email通知，并勾选"Send notifications for failed workflows only"。
+8. 在 GitHub 个人设置页面的 Notifications 下可以设置 GitHub Actions的通知，建议打开 Email 通知，并勾选 "Send notifications for failed workflows only"。
 
 ## data.json 数据获取方法
 
 使用 F12 开发者工具抓包之后得到数据，按照 json 格式写入 `data.json` 中。
 
-1. 登录进入 `http://weixine.自动打码.edu.cn/2020/`，打开开发者工具（Chrome 可以使用 F12 快捷键），选中 Network 窗口：
+1. 登录进入 `https://weixine.自动打码.edu.cn/2020/`，打开开发者工具（Chrome 可以使用 F12 快捷键），选中 Network 窗口：
 
 ![](./imgs/1.png)
 
@@ -49,7 +48,7 @@
 
 3. 将找到的 Data 除 `_token` （每次都会改变，所以不需要复制，脚本中会每次获取新的 token 并添加到要提交的数据中）外都复制下来，存放在 `data.json` 中，并参考示例文件转换为对应的格式。
 
-4. 通过push操作触发构建任务，检查上报数据是否正确。
+4. 通过 push 操作触发构建任务，检查上报数据是否正确。
 
 ## 许可
 
@@ -59,21 +58,10 @@ Copyright (c) 2020 BwZhang
 
 Copyright (c) 2020 Violin Wang
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Copyright (c) 2021 apylers
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- report.py
+++ report.py
@@ -1,101 +1,92 @@
-# encoding=utf8
-import requests
+import argparse
 import json
-import time
-import datetime
 import pytz
 import re
-import sys
-import argparse
+import requests
+
+from datetime import datetime
+
 from bs4 import BeautifulSoup
 
+
 class Report(object):
-    def __init__(self, stuid, password, data_path):
-        self.stuid = stuid
+    def __init__(self, id, password, data):
+        self.id = id
         self.password = password
-        self.data_path = data_path
+        self.data = data
 
     def report(self):
         session = self.login()
         cookies = session.cookies
-        data = session.get(
-            "http://weixine.ustc.edu.cn/2020").text
-        data = data.encode('ascii','ignore').decode('utf-8','ignore')
-        soup = BeautifulSoup(data, 'html.parser')
-        token = soup.find("input", {"name": "_token"})['value']
 
-        with open(self.data_path, "r+") as f:
+        data = session.get("https://weixine.ustc.edu.cn/2020").text
+        data = data.encode("ascii", "ignore").decode("utf-8", "ignore")
+        soup = BeautifulSoup(data, "html.parser")
+        token = soup.find("input", {"name": "_token"})["value"]
+
+        with open(self.data, "r+") as f:
             data = f.read()
             data = json.loads(data)
-            data["_token"]=token
+            data["_token"] = token
 
-        headers = {
-            'authority': 'weixine.ustc.edu.cn',
-            'origin': 'http://weixine.ustc.edu.cn',
-            'upgrade-insecure-requests': '1',
-            'content-type': 'application/x-www-form-urlencoded',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'referer': 'http://weixine.ustc.edu.cn/2020/',
-            'accept-language': 'zh-CN,zh;q=0.9',
-            'Connection': 'close',
-            'cookie': 'PHPSESSID=' + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
-        }
+        cookies = "laravel_session=" + cookies.get("laravel_session")
 
-        url = "http://weixine.ustc.edu.cn/2020/daliy_report"
+        headers = {"cookie": cookies}
+
+        url = "https://weixine.ustc.edu.cn/2020/daliy_report"
         session.post(url, data=data, headers=headers)
-        data = session.get("http://weixine.ustc.edu.cn/2020").text
-        soup = BeautifulSoup(data, 'html.parser')
-        pattern = re.compile("2021-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
+        data = session.get("https://weixine.ustc.edu.cn/2020").text
+        soup = BeautifulSoup(data, "html.parser")
         token = soup.find(
-            "span", {"style": "position: relative; top: 5px; color: #666;"})
+            "span", {"style": "position: relative; top: 5px; color: #666;"}
+        ).text
+        pattern = re.compile("202[0-9]-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
+
         flag = False
-        if pattern.search(token.text) is not None:
-            date = pattern.search(token.text).group()
-            print("Latest report: " + date)
-            date = date + " +0800"
-            reporttime = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
-            timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
-            delta = timenow - reporttime
+        if date := pattern.search(token).group():
+            print("Latest report:", date)
+            date += " +0800"
+            report_time = datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
+            now_time = datetime.now(pytz.timezone("Asia/Shanghai"))
+            delta = now_time - report_time
             print(delta.seconds)
-            if delta.seconds < 120:
+            if delta.seconds < 60:
                 flag = True
-        if flag == False:
-            print("Report FAILED!")
-        else:
+
+        if flag:
             print("Report SUCCESSFUL!")
+        else:
+            print("Report FAILED!")
+
         return flag
 
     def login(self):
-        url = "https://passport.ustc.edu.cn/login?service=http%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin"
+        print("Login...")
+
+        url = "https://passport.ustc.edu.cn/login"
         data = {
-            'model': 'uplogin.jsp',
-            'service': 'http://weixine.ustc.edu.cn/2020/caslogin',
-            'username': self.stuid,
-            'password': str(self.password),
+            "model": "uplogin.jsp",
+            "service": "https://weixine.ustc.edu.cn/2020/caslogin",
+            "username": self.id,
+            "password": self.password,
         }
         session = requests.Session()
         session.post(url, data=data)
 
-        print("login...")
         return session
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='USTC nCov auto report script.')
-    parser.add_argument('data_path', help='path to your own data used for post method', type=str)
-    parser.add_argument('stuid', help='your student number', type=str)
-    parser.add_argument('password', help='your CAS password', type=str)
+    parser = argparse.ArgumentParser(description="USTC nCov auto report script.")
+    parser.add_argument("data", help="Path to your own data", type=str)
+    parser.add_argument("id", help="Student ID", type=str)
+    parser.add_argument("password", help="Password", type=str)
     args = parser.parse_args()
-    autorepoter = Report(stuid=args.stuid, password=args.password, data_path=args.data_path)
-    count = 3
-    while count != 0:
-        ret = autorepoter.report()
-        if ret != False:
+    auto_reporter = Report(id=args.id, password=args.password, data=args.data)
+
+    for i in range(3):
+        if auto_reporter.report():
             break
         print("Report Failed, retry...")
-        count = count - 1
-    if count != 0:
-        exit(0)
     else:
         exit(-1)
